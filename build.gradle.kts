a6f2378725ccf05d610db952beacfc393b7af9d2
iseki <admin@iseki.space>
2024-12-05 12:52:44 +0800

    chore: upgrade main module Kotlin 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.9.0" apply false
+    kotlin("jvm") version "2.0.20" apply false
     kotlin("plugin.serialization") version "1.9.0" apply false
     id("org.jetbrains.dokka") version "1.9.0" apply false
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3" apply false
@@ -20,14 +18,5 @@ allprojects {
         isReproducibleFileOrder = true
     }
 
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.freeCompilerArgs += "-Xno-param-assertions"
-        kotlinOptions.freeCompilerArgs += "-Xno-call-assertions"
-        kotlinOptions.freeCompilerArgs += "-Xcontext-receivers"
-        kotlinOptions.freeCompilerArgs += "-Xno-receiver-assertions"
-        kotlinOptions.freeCompilerArgs += "-Xno-source-debug-extension"
-        kotlinOptions.freeCompilerArgs += "-Xassertions=jvm"
-        kotlinOptions.freeCompilerArgs += "-Xlambdas=indy"
-    }
 }
 
