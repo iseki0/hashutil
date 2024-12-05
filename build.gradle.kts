d868bb3c75686dfe2d3cf657289193d83ca6dd22
iseki <admin@iseki.space>
2024-12-05 15:38:32 +0800

    chore: upgrade plugin.serialization 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20" apply false
-    kotlin("plugin.serialization") version "1.9.0" apply false
+    kotlin("plugin.serialization") version "2.0.20" apply false
     id("org.jetbrains.dokka") version "1.9.0" apply false
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3" apply false
 }
